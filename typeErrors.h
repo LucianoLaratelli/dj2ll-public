3fb932c8a4f91856826f8a2e6eeffdd00bccffc4
Luciano E. Laratelli <luciano@laratelli.com>
2020-05-18 14:22:14 -0400

    extract type error reporters into typErrors.c/.h

---
+++ typeErrors.h
@@ -0,0 +1,21 @@
+#ifndef DJ2LL_TYPE_ERROR_HEADER
+#define DJ2LL_TYPE_ERROR_HEADER
+
+#ifdef __cplusplus
+{
+#endif
+
+#include "util.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+  void reportSemanticError(ASTree * t, char *argumentModifier, ASTree *badChild,
+                           int badType, char *expected);
+  void reportDuplicateField(char *varName, char *className, char *superClass,
+                            int varLine, int superClassVarLine);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
